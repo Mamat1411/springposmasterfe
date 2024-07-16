45f2c21cba3df3c4c1b5a1407c137c1419f19154
Mamat1411 <muhammadmujahid14@gmail.com>
2024-07-17 05:59:31 +0700

    Initial Commit

---
+++ src/main/java/com/xa/posmasterfe/PosmasterfeApplication.java
@@ -0,0 +1,13 @@
+package com.xa.posmasterfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosmasterfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosmasterfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xa/posmasterfe/PosmasterfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xa.posmasterfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosmasterfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
