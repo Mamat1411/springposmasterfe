4860608b5dd7d4eee8570888ba2a7dde509e9b5f
Mamat1411 <muhammadmujahid14@gmail.com>
2024-08-06 10:49:58 +0700

    Temporary Root Path Before Login and Register Implementation

---
+++ src/main/java/com/xa/posmasterfe/home/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.xa.posmasterfe.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+    
+    @GetMapping("")
+    public ModelAndView home() {
+        ModelAndView view = new ModelAndView("home/index");
+        return view;
+    }
+    
+}
